aebdade999247981b32585181c8bbf3ffec6d105
neel1996 <47709856+neel1996@users.noreply.github.com>
2024-04-23 08:05:27 +0530

    Update embedding function and ingestion pipeline

--- supabase/functions/embedDescription/index.ts
+++ supabase/functions/embedDescription/index.ts
@@ -1,43 +1,35 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import {
-  env,
-  pipeline,
-} from "https://cdn.jsdelivr.net/npm/@xenova/transformers@2.5.0";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-env.useBrowserCache = false;
-env.allowLocalModels = false;
-
-const supabaseClient = createClient(
-  Deno.env.get("SUPABASE_URL") ?? "",
-  Deno.env.get("SUPABASE_ANON_KEY") ?? ""
-);
-
-const pipe = await pipeline("feature-extraction", "Supabase/gte-small");
+const session = new Supabase.ai.Session("gte-small");
+const tableName = "description_embeddings";
 
 serve(async (req) => {
   const data = await req.json();
-  const tableName = "description_embeddings";
   console.log("Received request: ", data);
+
+  const authHeader = req.headers.get("Authorization")!;
+  const supabaseClient = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+    { global: { headers: { Authorization: authHeader } } }
+  );
 
   const {
     record: { id, description },
   } = data;
 
-  const output = await pipe(description, {
-    pooling: "mean",
+  const embeddings = await session.run(description, {
+    mean_pool: true,
     normalize: true,
   });
-  const description_embeddings = Array.from(output.data);
 
   const payload = {
     id,
-    description_embeddings,
+    embeddings,
   };
-
   const res = await supabaseClient.from(tableName).insert(payload);
   const error = res?.error;
-
   if (error) {
     console.error(error);
     return error;
